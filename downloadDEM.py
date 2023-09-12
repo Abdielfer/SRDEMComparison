f3db5e4045502800cd4b31f2ccd62b69ade2845f
Abdielfer <abdielfer@gmail.com>
2023-09-12 15:11:28 -0400

    downloadDEM

---
+++ downloadDEM.py
@@ -0,0 +1,14 @@
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+import hydra 
+from omegaconf import DictConfig
+import util as U
+
+
+@hydra.main(version_base=None, config_path=f"hydraConfig", config_name="mainConfig")
+def main(cfg: DictConfig):
+    U.reportSResDEMComparison(cfg)
+    
+if __name__ == "__main__":
+    with U.timeit():
+        main()  
